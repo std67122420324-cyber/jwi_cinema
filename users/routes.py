d52dff043e9f24760c803289180679227794e0b5
std67122420324-cyber <std.67122420324@ubru.ac.th>
2026-03-20 17:56:11 +0700

    Edit flie

--- users/routes.py
+++ users/routes.py
@@ -8,7 +8,7 @@ from models import User, Movie
 users_bp = Blueprint('users', __name__, template_folder='templates')
 
 def save_profile_picture(form_picture):
-    random_hex = secrets.token_hex(8)
+    random_hex = secrets.token_hex(7)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
